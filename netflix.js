60fe704a493ceaefc5be0eda82987ce848cf1938
Assimilationstheorie <konstantin@leafhouse.de>
2021-07-27 18:25:14 +0200

    Optim

--- netflix.js
+++ netflix.js
@@ -1,9 +1,6 @@
-function netflix()
-{
-    if(document.getElementsByClassName("skip-credits").length > 0)
-    {
+function netflix() {
+    if(document.getElementsByClassName("skip-credits").length > 0) {
         document.getElementsByClassName("skip-credits")[0].children[0].click();
     }
 }
-
 setInterval(netflix, 3000);
