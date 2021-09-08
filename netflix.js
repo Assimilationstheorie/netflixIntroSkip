dd14620472ff51979f305e778ba7916fe1373e84
Assimilationstheorie <konstantin@leafhouse.de>
2021-09-08 21:19:45 +0200

    Update to new Class

--- netflix.js
+++ netflix.js
@@ -1,6 +1,6 @@
 function netflix() {
-    if(document.getElementsByClassName("skip-credits").length > 0) {
-        document.getElementsByClassName("skip-credits")[0].children[0].click();
+    if(document.getElementsByClassName("ltr-1vr1ods").length > 0) {
+        document.getElementsByClassName("ltr-1vr1ods")[0].click()
     }
 }
 setInterval(netflix, 3000);
