bb41d71f876508c191ef95b89f3a69892c37f94b
Assimilationstheorie <konstantin@leafhouse.de>
2021-09-20 23:10:08 +0200

    Update script

--- netflix.js
+++ netflix.js
@@ -1,6 +1,6 @@
 function netflix() {
-    if(document.getElementsByClassName("ltr-1vr1ods").length > 0) {
-        document.getElementsByClassName("ltr-1vr1ods")[0].click()
+    if(document.getElementsByClassName("ltr-18i00qw").length > 0) {
+        document.getElementsByClassName("ltr-18i00qw")[0].click()
     }
 }
 setInterval(netflix, 3000);
